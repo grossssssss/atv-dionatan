fa7d2aa5f1ad3c7b1686001afabba72a28fb8f6e
João Gustavo Gross <engs-joaomatos@camporeal.edu.br>
2026-09-13 12:32:40 -0300

    ATV 14 - Criar formulario de cadastro de Aluno

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -6,7 +6,50 @@
 
     <h2>Cadastrar Aluno</h2>
 
-    <p>Aqui será criado o formulário de cadastro do aluno.</p>
+    <form action="{{ route('alunos.store') }}" method="POST">
+
+        @csrf
+
+        <div>
+            <label for="nome">Nome:</label>
+            <input
+                type="text"
+                id="nome"
+                name="nome"
+            >
+        </div>
+
+        <br>
+
+        <div>
+            <label for="email">Email:</label>
+            <input
+                type="email"
+                id="email"
+                name="email"
+            >
+        </div>
+
+        <br>
+
+        <div>
+            <label for="curso">Curso:</label>
+            <input
+                type="text"
+                id="curso"
+                name="curso"
+            >
+        </div>
+
+        <br>
+
+        <button type="submit">
+            Cadastrar
+        </button>
+
+    </form>
+
+    <br>
 
     <a href="{{ route('alunos.index') }}">
         Voltar
