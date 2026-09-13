518254b2b93ccb10a8864ca2e2678a5318baa9b8
João Gustavo Gross <engs-joaomatos@camporeal.edu.br>
2026-09-13 11:44:47 -0300

    ATV 1 - Criar rotas básicas

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::get('/sobre', function () {
+    return 'Página Sobre';
+});
+
+Route::get('/alunos', function () {
+    return 'Página de Alunos';
+});
+
+Route::get('/contato', function () {
+    return 'Página de Contato';
+});
 });
