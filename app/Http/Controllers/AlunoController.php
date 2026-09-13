c0a8cdcde4de0e28399713834fbc73e85bac1a4b
João Gustavo Gross <engs-joaomatos@camporeal.edu.br>
2026-09-13 11:51:19 -0300

    ATV 4 - Implementar rotas CRUD

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+    public function create()
+    {
+        return 'Formulário para cadastrar aluno';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Aluno cadastrado';
+    }
+
+    public function show(string $id)
+    {
+        return "Detalhes do aluno: $id";
+    }
+
+    public function edit(string $id)
+    {
+        return "Editar aluno: $id";
+    }
+
+    public function update(Request $request, string $id)
+    {
+        return "Aluno $id atualizado";
+    }
+
+    public function destroy(string $id)
+    {
+        return "Aluno $id excluído";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,9 +11,7 @@ Route::get('/sobre', function () {
     return 'Página Sobre';
 });
 
-Route::get('/alunos', function () {
-    return 'Página de Alunos';
-});
+Route::resource('alunos', AlunoController::class);
 
 Route::get('/contato', function () {
     return 'Página de Contato';
